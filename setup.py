e10ee9581b7dbceb76ca5742d5fb8f5182af65ba
ajheaps <andy.heaps@ncas.ac.uk>
2021-06-08 14:38:18 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-from distutils.core import setup, Extension
-from distutils.command.build import build
-
-packages     = ["cf"]
+from setuptools import setup
 
 setup(name = "cf-view",
-      long_description = "GUI for viewing CF files",
-      version      = "2.0.2",
-      description  = "GUI for viewing CF files",
-      author       = "Andy Heaps",
-      maintainer   = "Andy Heaps",
-      maintainer_email  = "andy.heaps@ncas.ac.uk",
-      author_email = "andy.heaps@ncas.ac.uk",
-      url          = "http://ajheaps.github.io/cf-view",
-      download_url = "https://github.com/ajheaps/CF-View",
-      platforms    = ["Linux"],
-      install_requires = ["cf-python >= 3.8.0",
-                          "cf-plot >= 3.1.6",
-                          "pyqt >= 5.5.0",
-                          ],
-      keywords     = ["cf","netcdf","data","science",
-                      "oceanography","meteorology","climate"],
-      scripts      = ["cfview"]
+    packages = "cfview",
+    long_description = "GUI for viewing CF files",
+    version      = "2.0.3",
+    description  = "GUI for viewing CF files",
+    author       = "Andy Heaps",
+    maintainer   = "Andy Heaps",
+    maintainer_email  = "andy.heaps@ncas.ac.uk",
+    author_email = "andy.heaps@ncas.ac.uk",
+    url          = "http://ajheaps.github.io/cf-view",
+    download_url = "https://github.com/ajheaps/CF-View",
+    platforms    = ["Linux", "MacOS"],
+    license="MIT",
+    install_requires = ["cf-python>=3.8.0",
+                        "cf-plot>=3.1.6",
+                        "pyqt>=5.5.0",
+                        ],
+    keywords     = ["cf","netcdf","data","science",
+                    "oceanography","meteorology","climate"],
+    scripts      = ["cfview"]
   )
 
 
